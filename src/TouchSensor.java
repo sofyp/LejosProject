25cbcd90d33d63a7a211194827b839f940d8e0c3
sofyp <36504521+sofyp@users.noreply.github.com>
2018-02-26 09:30:03 +0200

    Update TouchSensor.java

--- src/TouchSensor.java
+++ src/TouchSensor.java
@@ -2,6 +2,28 @@
 public class TouchSensor {
 
 	public TouchSensor() {
-		
+				
+	EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S3); 
+	SensorMode touch = touchSensor.getTouchMode(); 
+	float[] sample = new float[touch.sampleSize()];
+	Motors motors = new Motors();
+	Player player = new Player();
+	InfraredSensor infra= new InfraredSensor();
+	
+	while (!Button.ESCAPE.isDown()) {
+	
+		touch.fetchSample(sample, 0);
+	
+		if (sample[0]==1) {
+			motors.stopMotors();
+			player.playMyHeart(100);
+			if (infra.waitRemoteCommand()) {
+				motors.start();
+			}
+		}
+	}
+	touchSensor.close();	  
 	}
 }
+		
+
